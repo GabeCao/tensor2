6142283e3fa23d1c1fb779fe37318e00c401a9f1
GabeCao <caoxianbozjzx@163.com>
2018-06-09 15:31:48 +0800

    inital

--- Evn.py
+++ Evn.py
@@ -1,4 +1,6 @@
 from sklearn.preprocessing import LabelBinarizer
+import numpy as np
+import os
 class Evn:
     def __init__(self):
         self.state = []
@@ -8,12 +10,93 @@ class Evn:
         self.sensors = []
         # 初始化self.sensors_mobile_charger 和 self.sensors
         self.set_sensors_mobile_charger()
+        # 对剩余寿命进行独热编码
+        self.rl = ['Greater than the threshold value, 0', 'Smaller than the threshold value, 1', 'dead, -1']
+        self.rl_label_binarizer = LabelBinarizer()
+        self.rl_one_hot_encoded = self.rl_label_binarizer.fit_transform(self.rl)
+        # 对是否属于hotspot 独热编码
+        self.belong = ['1', '0']
+        self.belong_label_binarizer = LabelBinarizer()
+        self.belong_one_hot_encoded = self.belong_label_binarizer.fit_transform(self.belong)
+        # 对action 独热编码,用字典表示,key: action, value: action的编码结果
+        self.action_one_hot_encoded_8 = {}
+        self.action_one_hot_encoded_9 = {}
+        self.action_one_hot_encoded_10 = {}
+        self.action_one_hot_encoded_11 = {}
+        self.action_one_hot_encoded_12 = {}
+        self.action_one_hot_encoded_13 = {}
+        self.action_one_hot_encoded_14 = {}
+        self.action_one_hot_encoded_15 = {}
+        self.action_one_hot_encoded_16 = {}
+        self.action_one_hot_encoded_17 = {}
+        self.action_one_hot_encoded_18 = {}
+        self.action_one_hot_encoded_19 = {}
+        self.action_one_hot_encoded_20 = {}
+        self.action_one_hot_encoded_21 = {}
 
-        self
+    def action_to_one_hot_encoded(self, path, file, action_one_hot_encoded):
+        # 存放每个文件中的所有hotspot
+        hotspots = []
+        # 存放每个hotspot 对应的最大等待时间
+        hotspot_max_staying_time = {}
+        with open(path + file) as f:
+            for line in f:
+                data = line.strip().split(',')
+                hotspots.append(data[0])
+                hotspot_max_staying_time[data[0]] = data[1]
+        # 对hotpsots 进行独热编码
+        label_binarizer = LabelBinarizer()
+        hotspot_one_hot_encoded = label_binarizer.fit_transform(hotspots)
+        # 获取独热编码后的行和列
+        rows, cols = hotspot_one_hot_encoded.shape
 
+        for row in range(rows):
+            # 获得独热编码前的hotspot 编号，用这个编号在hotspot_max_staying_time 找到其最大等待时间
+            hotspot = label_binarizer.inverse_transform(hotspot_one_hot_encoded[[row]])
+            for key, value in hotspot_max_staying_time.items():
+                if hotspot == key:
+                    max_staying_time = int(hotspot_max_staying_time[key])
+                    # 找到最大等待时间以后，对独热编码后的当前行后面添加一个等待时间(从1到最大等待时间)
+                    for i in range(1, max_staying_time + 1):
+                        tmp = hotspot_one_hot_encoded[row]
+                        tmp = np.r_[tmp, i]
+                        action_one_hot_encoded[hotspot + ',' + str(i)] = tmp
 
-    def sensors_one_hot_encoded(self):
-
+    def set_action_one_hot_encoded(self):
+        # 保存最大等待时间的文件夹
+        path = '五分钟/'
+        files = os.listdir(path)
+        # 对于每个文件,读取文件的内容, 进行热编码放入相应的字典中。例如8.txt代表8时间段的内容，
+        # 放入到action_one_hot_encoded_8字典中
+        for file in files:
+            if file == '8.txt':
+                self.action_to_one_hot_encoded(path, file, self.action_one_hot_encoded_8)
+            elif file == '9.txt':
+                self.action_to_one_hot_encoded(path, file, self.action_one_hot_encoded_9)
+            elif file == '10.txt':
+                self.action_to_one_hot_encoded(path, file, self.action_one_hot_encoded_10)
+            elif file == '11.txt':
+                self.action_to_one_hot_encoded(path, file, self.action_one_hot_encoded_11)
+            elif file == '12.txt':
+                self.action_to_one_hot_encoded(path, file, self.action_one_hot_encoded_12)
+            elif file == '13.txt':
+                self.action_to_one_hot_encoded(path, file, self.action_one_hot_encoded_13)
+            elif file == '14.txt':
+                self.action_to_one_hot_encoded(path, file, self.action_one_hot_encoded_14)
+            elif file == '15.txt':
+                self.action_to_one_hot_encoded(path, file, self.action_one_hot_encoded_15)
+            elif file == '16.txt':
+                self.action_to_one_hot_encoded(path, file, self.action_one_hot_encoded_16)
+            elif file == '17.txt':
+                self.action_to_one_hot_encoded(path, file, self.action_one_hot_encoded_17)
+            elif file == '18.txt':
+                self.action_to_one_hot_encoded(path, file, self.action_one_hot_encoded_18)
+            elif file == '19.txt':
+                self.action_to_one_hot_encoded(path, file, self.action_one_hot_encoded_19)
+            elif file == '20.txt':
+                self.action_to_one_hot_encoded(path, file, self.action_one_hot_encoded_20)
+            elif file == '21.txt':
+                self.action_to_one_hot_encoded(path, file, self.action_one_hot_encoded_21)
 
     def set_sensors_mobile_charger(self):
         self.sensors_mobile_charger['000'] = [0.7 * 10.8 * 1000, 0.6, '08:00:00']
@@ -60,5 +143,5 @@ class Evn:
 
 if __name__ == '__main__':
     evn = Evn()
-    evn.reset()
-    print(evn.state)
+    evn.set_action_one_hot_encoded()
+
--- index2.py
+++ index2.py
@@ -1,20 +1,35 @@
 import os
 from sklearn.preprocessing import LabelBinarizer
+import numpy as np
 if __name__ == '__main__':
+    label_binarizer = LabelBinarizer()
     path = '五分钟/'
     files = os.listdir(path)
-    max_staying_time = {}
-    for file in files:
-        name = file.split('.')[0]
-        lines = []
-        with open(path + file) as f:
-            for line in f:
-                lines.append(line.strip())
-        max_staying_time[name] = lines
-    print(max_staying_time[str(8)])
-    label_binarizer = LabelBinarizer()
-    one = label_binarizer.fit_transform(max_staying_time[str(8)])
-    row, col = one.shape
-    for i in range(row):
-        print('......')
-        print(one[i])
+    hotspot_max_staying_time = {}
+    hotspots = []
+    file = files[0]
+    print(file)
+    with open(path + file) as f:
+        for line in f:
+            data = line.strip().split(',')
+            hotspots.append(data[0])
+            hotspot_max_staying_time[data[0]] = data[1]
+    hotspot_one_hot_encoded = label_binarizer.fit_transform(hotspots)
+
+    rows, cols = hotspot_one_hot_encoded.shape
+    hotspot_max_staying_time_one_hot_encoded = np.zeros(shape=[rows * 12, cols + 1], dtype=np.int32)
+    index = 0
+    for row in range(rows):
+        hotspot = label_binarizer.inverse_transform(hotspot_one_hot_encoded[[row]])
+        for key, value in hotspot_max_staying_time.items():
+            if hotspot == key:
+                max_staying_time = int(hotspot_max_staying_time[key])
+                for i in range(1, max_staying_time + 1):
+                    # a = hotspot_one_hot_encoded[row]
+                    # a = np.r_[a, i]
+                    # print(a)
+                    a = hotspot_one_hot_encoded[row]
+                    a = np.r_[a, i]
+                    hotspot_max_staying_time_one_hot_encoded[index] = a
+                    index += 1
+    hotspot_max_staying_time_one_hot_encoded = hotspot_max_staying_time_one_hot_encoded[:index]
